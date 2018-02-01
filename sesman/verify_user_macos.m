399088ce30535a4600afadcce7fa8c28dbb614de
Vraiment <jemc44@gmail.com>
2018-02-01 00:29:52 -0800

    Reorganized variables and used underscore notation

--- sesman/verify_user_macos.m
+++ sesman/verify_user_macos.m
@@ -42,27 +42,31 @@
 long
 auth_userpass(const char *user, const char *pass, int *errorcode)
 {
-    NSString *userName = [NSString stringWithCString:user
-                                   encoding:[NSString defaultCStringEncoding]];
-    NSString *password = [NSString stringWithCString:pass
-                                   encoding:[NSString defaultCStringEncoding]];
+    NSStringEncoding encoding;
+    NSString *user_name;
+    NSString *password;
+    CBIdentityAuthority *identity_authority;
+    CBIdentity *identity;
+    CBUserIdentity *user_identity;
+
+    encoding = [NSString defaultCStringEncoding];
+    user_name = [NSString stringWithCString:user encoding:encoding];
+    password = [NSString stringWithCString:pass encoding:encoding];
 
     /*
      * If the identity should be grabed for network (like LDAP)
      */
-    CBIdentityAuthority *localAuthority =
-        [CBIdentityAuthority localIdentityAuthority];
-
-    CBIdentity *identity = [CBIdentity identityWithName:userName
-                                       authority:localAuthority];
+    identity_authority = [CBIdentityAuthority localIdentityAuthority];
+    identity = [CBIdentity identityWithName:user_name
+                           authority:identity_authority];
     if (identity == nil || ![identity isKindOfClass:[CBUserIdentity class]])
     {
         return 0;
     }
 
-    CBUserIdentity *userIdentity = (CBUserIdentity *) identity;
-    return userIdentity.isEnabled &&
-           [userIdentity authenticateWithPassword:password];
+    user_identity = (CBUserIdentity *) identity;
+    return user_identity.isEnabled &&
+        [user_identity authenticateWithPassword:password];
 }
 
 int
